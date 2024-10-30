c64ebd82b667f72d301cabb131818020e5648619
Bzzyku <patrykdlugolecki@o2.pl>
2024-10-30 08:03:46 +0100

    divide code into more files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,60 +1,12 @@
+import { displayProducts } from "./display-products.js";
+import { products } from "../data/products.js";
+import { cart } from "../data/cart.js";
 
-let gridHTML = ''
-products.forEach(element => {
-  gridHTML+= 
- ` <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src=${element.image}>
-          </div>
 
-          <div class="product-name limit-text-to-2-lines">
-            ${element.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${element.rating.stars*10}.png">
-            <div class="product-rating-count link-primary">
-              ${element.rating.count}
-            </div>
-          </div>
-
-          <div class="product-price">
-            $${(element.priceCents/100).toFixed(2)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector-${element.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart js-added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${element.id}"
-          data-product-name="${element.name}">
-            Add to Cart
-          </button>
-        </div>`
-});
-document.querySelector('.products-grid-js').innerHTML = gridHTML;
+displayProducts(products);
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+
   button.addEventListener('click',() => {
     const productId = button.dataset.productId;
     const productName = button.dataset.productName;
@@ -84,3 +36,5 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     }, 2000)
   })
 })
+
+
