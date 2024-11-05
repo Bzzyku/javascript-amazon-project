fb5c127d32f3d1a64d1721f0254750f5fcb4513a
Bzzyku <patrykdlugolecki@o2.pl>
2024-11-05 12:07:09 +0100

    the delete button in sorking now and its using local storage

---
+++ scripts/cash.js
@@ -0,0 +1,3 @@
+export function parseCents(money){
+ return (parseInt(money)/100).toFixed(2)
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,44 +1,35 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
-
+import { parseCents } from "./cash.js";
 displayBasketProducts();
 
+deleteButtonOnClick();
 
 
-
-function prepareCart () {
-  
-    
-    const cartData = JSON.parse(localStorage.getItem('cart'));
-    console.log(cartData)
-    console.log(products)
-    let tmp;
-    return products.map((product) => {
-      tmp = cartData.filter((element) => {
-        element.productId === product.id
-      })
-        return {
-          id: product.id,
-          image: product.image,
-          name: product.name,
-          quantity: tmp.quantity,
-          priceCents: product.priceCents,
-        }
-      
-    
+function prepareCart() {
+  const cartData = JSON.parse(localStorage.getItem('cart'));
+  return cartData.map((e) => {
+    const product = products.find((x) => e.productId === x.id);
+    if (product) {
+      return {
+        id: product.id,
+        name: product.name,
+        image: product.image,
+        priceCents: product.priceCents,
+        quantity: e.quantity 
+      };
     }
-  )
+  }) 
 }
 
 
 
 
 function displayBasketProducts () {
-  let fuckingcart = prepareCart();
-  console.log(fuckingcart)
+  
   let HTML = ''
-  fuckingcart.forEach((cartItem) => {
+  console.log(prepareCart())
+  prepareCart().forEach((cartItem) => {
     HTML += `
     <div class="cart-item-container-${cartItem.id}">
             <div class="delivery-date">
@@ -54,7 +45,7 @@ function displayBasketProducts () {
                   Black and Gray Athletic Cotton Socks - 6 Pairs
                 </div>
                 <div class="product-price">
-                  $${cartItem.quantity*cartItem.priceCents}
+                  $${parseCents(cartItem.quantity*cartItem.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -63,7 +54,7 @@ function displayBasketProducts () {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-quantity-link" data-product-id=${cartItem.id}>
                     Delete
                   </span>
                 </div>
@@ -76,7 +67,7 @@ function displayBasketProducts () {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${cartItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -89,7 +80,7 @@ function displayBasketProducts () {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${cartItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -102,7 +93,7 @@ function displayBasketProducts () {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${cartItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -121,3 +112,26 @@ function displayBasketProducts () {
   }
 
 
+  function deleteButtonOnClick() {
+    document.querySelectorAll(`.js-delete-quantity-link`).forEach((link) => {
+      link.addEventListener('click',() => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+      })
+    })
+  }
+
+  function removeFromCart(productId) {
+    const newCart = [];
+    console.log(productId)
+    
+    JSON.parse(localStorage.getItem(`cart`)).forEach((e) => {
+      if(e.productId !== productId){
+        newCart.push(e);
+      }
+    })
+    console.log(newCart)
+    localStorage.setItem('cart',JSON.stringify(newCart));
+    
+    document.querySelector(`.cart-item-container-${productId}`).remove();
+  }
