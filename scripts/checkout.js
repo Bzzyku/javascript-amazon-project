db02c7a189d188087dae94f95e2e1b0e86748f39
Bzzyku <patrykdlugolecki@o2.pl>
2024-11-06 18:00:28 +0100

    Adding features like displaying delivery date in delivery options according to current time

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,37 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { parseCents } from "./cash.js";
+import { deliveryTime } from "./delivery-time.js";
+
+
+function deliveryOptionsHTML(cartItem) {
+
+  let HTML = '';
+  deliveryTime.forEach((e) => {
+    const currentTime = dayjs();
+    const date = currentTime.add(e.deliveryDays, 'day')
+    const price = e.id == 1
+    ? 'FREE Shipping'
+    : `$${parseCents(e.priceCents)} - Shipping`;
+  HTML +=
+    `
+        <div class="delivery-option">
+          <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-${cartItem.id}">
+          <div>
+            <div class="delivery-option-date">
+              ${date.format('dddd, MMMM D')}
+            </div>
+            <div class="delivery-option-price">
+              ${price}
+            </div>
+          </div>
+        </div>  
+    `
+  })
+  return HTML;
+}
 displayBasketProducts();
 
 deleteButtonOnClick();
@@ -64,45 +95,7 @@ function displayBasketProducts () {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${cartItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                ${deliveryOptionsHTML(cartItem)}
               </div>
             </div>
           </div>`
